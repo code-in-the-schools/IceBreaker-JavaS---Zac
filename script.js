567ecf2452786162911b11d0ca61fae0938274f8
Zachiah Zook <zakhazook1@gmail.com>
2020-07-30 14:51:49 +0000

    things

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
+console.log (Date())
+
 function setup() {
 	createCanvas(500, 400);
 }
 
-var [xrot, yrot, t] = [350, 200, 1];
+var [xrot, yrot, t] = [200, 100, 1];
 
 function draw() {
-	background(200);
 	
 	// thing you move
-	fill(0, 255, 0);
-	ellipse(mouseX, mouseY, 50);
+	//fill(0, 110, 115);
+	//ellipse(mouseX, mouseY, 50);
 	
 	// big wheel
 	fill(255);
-	ellipse(150, 150, 100);
+	ellipse(200, 150, 150);
 	
 	// weird moving line
 	fill(0);
 	strokeCap(ROUND);
-	line(mouseX, mouseY, xrot+50, yrot+45);
-	
+	line(mouseX, mouseY, xrot+50, yrot+45)
+var i;
+for (i=0; i<25; i++){
+  console.log("weird");
+}
+ 
+
+
 	// make it spin
 	xrot += Math.cos((PI/2) * t) * 8;
 	yrot += Math.sin((PI/2) * t) * 8;
